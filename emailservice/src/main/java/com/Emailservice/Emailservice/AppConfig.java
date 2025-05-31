19deff99d931e009c0d7b2c1dc212970aea506ea
unknown <chaitunadagouda@gmail.com>
2025-05-31 15:58:53 +0530

    add environment variables

--- emailservice/src/main/java/com/Emailservice/Emailservice/AppConfig.java
+++ emailservice/src/main/java/com/Emailservice/Emailservice/AppConfig.java
@@ -1,8 +1,10 @@
 package com.Emailservice.Emailservice;
 
 
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Value;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +14,17 @@ import org.springframework.context.annotation.Configuration;
 @Getter
 @Setter
 public class AppConfig {
+
     private String username;
     private String password;
     private String persnalName;
+
+    @PostConstruct
+    public void print() {
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
+
 
     // Getters and Setters
     public String getUsername() { return username; }
